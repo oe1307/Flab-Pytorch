aca545df832e61a4fb3c45090ba3c793f2aeef2d
oe1307 <oe.issa.392@s.kyushu-u.ac.jp>
2023-04-20 14:24:27 +0900

    update

--- src/data_loader.py
+++ src/data_loader.py
@@ -5,11 +5,12 @@ from load_dataset import MyDataset
 if __name__ == "__main__":
     # below is the example of how to use DataLoader
 
-    dataset = MyDataset("../storage/imagenet/val/*/*.JPEG")
+    dataset = MyDataset("../storage/val/*/*.JPEG")
     dataloader = DataLoader(dataset, batch_size=10, shuffle=True)
 
-    for batch, (data, label) in enumerate(dataloader):
+    for batch, (data, label, index) in enumerate(dataloader):
         print(data)
         print(data.shape)
         print(label)
+        print(index)
         break
--- src/load_dataset.py
+++ src/load_dataset.py
@@ -1,18 +1,21 @@
+import json
 import os
 from glob import glob
+from urllib.request import urlretrieve
 
 from PIL import Image
 from torch.utils.data import Dataset
 from torchvision import transforms as T
-from torchvision.datasets import ImageNet
+from torchvision.datasets.imagenet import parse_devkit_archive, parse_val_archive
 
 
 class MyDataset(Dataset):
     def __init__(self, path):
-        """
-        set path of images and transform
-        """
+        """set path of images and transform"""
         self.path = glob(path)
+        class_index = json.load(open("../storage/imagenet_class_index.json", "r"))
+        self.label2index = {v[0]: int(k) for k, v in class_index.items()}
+        self.n_classes = len(self.label2index)
         self.transform = T.Compose(
             [
                 T.Resize((224, 224)),
@@ -30,16 +33,28 @@ class MyDataset(Dataset):
         image = Image.open(self.path[idx]).convert("RGB")
         image = self.transform(image)
         label = self.path[idx].split("/")[-2]
-        return image, label
+        index = self.label2index[label]
+        return image, label, index
 
 
 if __name__ == "__main__":
-    if not os.path.exists("../storage/imagenet/val"):
+    if not os.path.exists("../storage/val"):
         print("Expanding imagenet dataset...")
-        ImageNet("../storage/imagenet", split="val")
+        parse_devkit_archive("../storage")
+        parse_val_archive("../storage")
+        urlretrieve(
+            "https://s3.amazonaws.com/deep-learning-models/"
+            + "image-models/imagenet_class_index.json",
+            "../storage/imagenet_class_index.json",
+        )
 
-    dataset = MyDataset("../storage/imagenet/val/*/*.JPEG")
-    data, label = dataset[0]
+    dataset = MyDataset("../storage/val/*/*.JPEG")
+    print("n_examples =", len(dataset))
+    print("n_classes =", dataset.n_classes)
+
+    # check 0th data
+    data, label, index = dataset[0]
     print(data)
     print(data.shape)
-    print(label)
+    print("label:", label)
+    print("index:", index)
