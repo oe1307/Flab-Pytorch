81e716cfc37f87ef091f1408a0bd3c15a02da115
oe1307 <oe.issa.392@s.kyushu-u.ac.jp>
2023-04-20 12:01:03 +0900

    update

--- src/data_loader.py
+++ src/data_loader.py
@@ -5,7 +5,7 @@ from load_dataset import MyDataset
 if __name__ == "__main__":
     # below is the example of how to use DataLoader
 
-    dataset = MyDataset("../storage/imagenet/val/*/*.JPEG")
+    dataset = MyDataset("../storage/val/*/*.JPEG")
     dataloader = DataLoader(dataset, batch_size=10, shuffle=True)
 
     for batch, (data, label) in enumerate(dataloader):
